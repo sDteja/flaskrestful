203deb8ac3bfad75ef8285f8513caddfb1a18568
Dharma Surisetti <dharma.surisetti@o9solutions.com>
2021-08-27 17:06:08 +0530

    red code removal

--- app.py
+++ app.py
@@ -15,12 +15,6 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 api = Api(app)
 
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 jwt = JWTManager(app)
 
 api.add_resource(Authenticate, "/auth")
