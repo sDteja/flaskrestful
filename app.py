126cbdf267a37e4bfd4903cda98bdeb0d7311285
Dharma Surisetti <dharma.surisetti@o9solutions.com>
2021-08-27 18:02:14 +0530

    changes for postgres

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
@@ -10,7 +11,7 @@ from db import db
 
 app = Flask('__name__')
 app.config["JWT_SECRET_KEY"] = "test"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL_NEW', "sqlite:///data.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 api = Api(app)
